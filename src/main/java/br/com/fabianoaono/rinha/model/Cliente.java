82de5cdf825545c45ec63ab4766c00e5510e083e
Fabiano Aono <fabianoaono@gmail.com>
2024-03-01 23:53:56 -0300

    Ommiting id from json response

--- src/main/java/br/com/fabianoaono/rinha/model/Cliente.java
+++ src/main/java/br/com/fabianoaono/rinha/model/Cliente.java
@@ -1,16 +1,24 @@
 package br.com.fabianoaono.rinha.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Cliente extends PanacheEntity {
+public class Cliente extends PanacheEntityBase {
 
+    @Id
+    @GeneratedValue
+    @JsonIgnore
+    public Long id;
     public Integer limite;
     public Integer saldo;
 }
